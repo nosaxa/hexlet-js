e6bf408a01d964f3ca0254b2c0cab84385827d7d
Maxim <nosaxa@protonmail.com>
2021-11-01 10:52:26 +0100

    Test prettier

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log('Hello Hexlet!');
-console.log(_.last(['one', 'two']));
+console.log("Hello Hexlet!");
+console.log(_.last(["one", "two"]));
+
+const subtr = (a, b) => {
+  if (a > b) {
+    return a - b;
+  } else {
+    return b - a;
+  }
+};
+
+const first = subtr(12, 4);
+const second = subtr(2399, 5550);
+
+console.log(first, second);
